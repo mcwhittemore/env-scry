9ac81379ec904f8f840596981455c68eed4dfb3d
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-07-25 23:35:26 -0400

    create a cli bin file to print the env vars to console by source module

---
+++ bin/cli.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+var envScry = require("../");
+
+envScry(process.cwd(), function(err, modulesByEnvVars){
+	var envVars = Object.keys(modulesByEnvVars);
+
+	var envVarsByTitle = {};
+
+	for(var i=0; i< envVars.length; i++){
+		var ev = envVars[i];
+		var title = "# from "+modulesByEnvVars[ev].join(", ");
+		envVarsByTitle[title] = envVarsByTitle[title] || [];
+		envVarsByTitle[title].push(ev+"=\"\"");
+	}
+
+	var titles = Object.keys(envVarsByTitle);
+
+	for(var i=0; i<titles.length; i++){
+		var title = titles[i];
+		console.log(title);
+		console.log(envVarsByTitle[title].join("\n"), "\n");
+	}
+
+});
