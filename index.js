03ea219fab785ead63220d487628458f3773a858
Dan MacTough <danmactough@gmail.com>
2015-08-17 22:48:32 -0400

    Add eslintrc and (mostly) lint existing code

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ var dir = require("node-dir");
 var path = require("path");
 var fs = require("fs");
 
-module.exports = function(folder, cb){
+module.exports = function(folder, cb) {
 
 	dir.files(folder, function(err, files) {
-		if(err){
+		if (err) {
 			cb(err);
 		}
-		else{
+		else {
 			var moduleNames = ["active"];
 			var filesByModule = {
 				active: []
 			};
-			for(var i=0; i<files.length; i++){
+			for (var i = 0; i < files.length; i++) {
 				var file = files[i].replace(folder, "");
 
-				if(file.match(/\.js$/)){
+				if (file.match(/\.js$/)) {
 					var bits = file.split(path.sep);
-					if(bits[1] == "node_modules"){
+					if (bits[1] === "node_modules") {
 						filesByModule[bits[2]] = filesByModule[bits[2]] || [];
 						filesByModule[bits[2]].push(files[i]);
 					}
-					else{
-						filesByModule["active"].push(files[i]);
+					else {
+						filesByModule["active"].push(files[i]); // eslint-disable-line dot-notation
 					}
 				}
 
-				if(file.match(/^\/node_modules\/[a-z\-\_]*\/package\.json$/)){
+				if (file.match(/^\/node_modules\/[a-z\-\_]*\/package\.json$/)) {
 					var moduleName = file.split(path.sep)[2];
 					moduleNames.push(moduleName);
 				}
@@ -35,19 +35,19 @@ module.exports = function(folder, cb){
 
 			var modulesByEnvVars = {};
 
-			for(var i=0; i<moduleNames.length; i++){
+			for (var i = 0; i < moduleNames.length; i++) {
 				var moduleName = moduleNames[i];
 
-				for(var j=0; j<filesByModule[moduleName].length; j++){
+				for (var j = 0; j < filesByModule[moduleName].length; j++) {
 					var file = filesByModule[moduleName][j];
 					var content = fs.readFileSync(file).toString();
-					
+
 					var envVars = content.match(/process\.env\.[a-zA-z_]*/);
-					if(envVars){
-						envVars.forEach(function(pev){
+					if (envVars) {
+						envVars.forEach(function(pev) {
 							var ev = pev.split(".")[2];
 							modulesByEnvVars[ev] = modulesByEnvVars[ev] || [];
-							if(modulesByEnvVars[ev].indexOf(moduleName) === -1){
+							if (modulesByEnvVars[ev].indexOf(moduleName) === -1) {
 								modulesByEnvVars[ev].push(moduleName);
 							}
 						});
@@ -58,4 +58,4 @@ module.exports = function(folder, cb){
 			cb(null, modulesByEnvVars);
 		}
 	});
-}
+};
