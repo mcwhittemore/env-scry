4c977c87d96a00a110c7f2e4707073dce421afbb
Dan MacTough <danmactough@gmail.com>
2015-08-19 23:39:12 -0400

    Refactor modules-by-env-var mapping
    I rewrote it with higher-order functions because I thought the nested for-loops were too confusing.
    
    It's still nested loops -- just in a more functional style.

--- index.js
+++ index.js
@@ -42,29 +42,37 @@ module.exports = function(folder, cb) {
 				}
 			}
 
-			var modulesByEnvVars = {};
-
-			for (var i = 0; i < moduleNames.length; i++) {
-				var moduleName = moduleNames[i];
-
-				for (var j = 0; j < filesByModule[moduleName].length; j++) {
-					var file = filesByModule[moduleName][j];
-					var content = fs.readFileSync(file).toString();
-
-					var envVars = content.match(/process\.env\.[a-zA-z_]*/);
-					if (envVars) {
-						envVars.forEach(function(pev) {
-							var ev = pev.split(".")[2];
-							modulesByEnvVars[ev] = modulesByEnvVars[ev] || [];
-							if (modulesByEnvVars[ev].indexOf(moduleName) === -1) {
-								modulesByEnvVars[ev].push(moduleName);
-							}
-						});
-					}
-				}
-			}
+			var modulesByEnvVars = moduleNames.reduce(function (map, moduleName) {
+				var mapModuleToEnvVars = createModuleToEnvVarsMapper(map, moduleName);
+				var files = filesByModule[moduleName];
+				files.forEach(function (file) {
+					mapEnvVarsFromFile(file, mapModuleToEnvVars);
+				});
+				return map;
+			}, {});
 
 			cbWrapper(null, modulesByEnvVars);
 		}
 	});
 };
+
+function createModuleToEnvVarsMapper (modulesByEnvVars, moduleName) {
+	return function mapModuleToEnvVars (envVar) {
+		modulesByEnvVars[envVar] = modulesByEnvVars[envVar] || [];
+		if (modulesByEnvVars[envVar].indexOf(moduleName) === -1) {
+			modulesByEnvVars[envVar].push(moduleName);
+		}
+	};
+}
+
+function mapEnvVarsFromFile (file, mapFn) {
+	var content = fs.readFileSync(file).toString();
+	var matches = content.match(/process\.env\.[a-zA-z_]*/);
+
+	if (matches) {
+		matches.forEach(function (match) {
+			var envVar = match.split(".")[2];
+			mapFn(envVar);
+		});
+	}
+}
